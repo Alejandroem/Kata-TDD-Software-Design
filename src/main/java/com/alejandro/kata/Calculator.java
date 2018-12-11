19eed3be763bbf3ed108b35107b227b259aa01b8
alejandroem <alexalejandroem@gmail.com>
2018-12-11 21:06:14 +0100

    Added code to fullfill the two numbers test  Changes to be committed: 	modified:   src/main/java/com/alejandro/kata/Calculator.java

--- src/main/java/com/alejandro/kata/Calculator.java
+++ src/main/java/com/alejandro/kata/Calculator.java
@@ -3,10 +3,12 @@ package com.alejandro.kata;
 public class Calculator {
 
 	public int addNumbersInString(String numbers) {
+        String regexDelimiter = ",";
         if(numbers.length() == 0){
             return 0;
         }
-		return -1;
+        String[] splittedNumbers = numbers.split(regexDelimiter);		
+        return Integer.parseInt(splittedNumbers[0]) + Integer.parseInt(splittedNumbers[1]);
 	}
     
 }
