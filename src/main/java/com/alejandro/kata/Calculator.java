cb36a9c7d0032fa344fe1470688b977b668bc1a9
alejandroem <alexalejandroem@gmail.com>
2018-12-11 20:36:13 +0100

    Added minimum code for the test to succeed  Changes to be committed: 	new file:   src/main/java/com/alejandro/kata/Calculator.java

---
+++ src/main/java/com/alejandro/kata/Calculator.java
@@ -0,0 +1,12 @@
+package com.alejandro.kata;
+
+public class Calculator {
+
+	public int addNumbersInString(String numbers) {
+        if(numbers.length() == 0){
+            return 0;
+        }
+		return -1;
+	}
+    
+}
